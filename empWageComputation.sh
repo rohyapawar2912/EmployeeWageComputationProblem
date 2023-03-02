eeb3d1fb353c816d83ab7d85b0380dbd3519cd7e
“rohyapawar2912” <rohitpawar8530@gmail.com>
2023-03-02 13:14:46 +0530

    Employee Present or AbsenT

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,11 @@
-echo "Welcome to Employee Wage Computation Program"
+echo "welcome to Employee Wage Computation Problem"
+isPresent=1;
+randomCheck=$((RANDOM%2));
+
+if [ $isPresent -eq $randomCheck ]
+then
+echo "Employee is present"
+else
+echo "Employee is absent"
+fi
+
